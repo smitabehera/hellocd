9f397d6d529d1abd070c59f1569ed2656f68efa3
smita <root@instance-2.asia-southeast1-b.c.ci-cd-275613.internal>
2020-12-18 19:32:26 +0000

    New changes

--- src/main/java/com/example/hellocd/NamehellocdApplication.java
+++ src/main/java/com/example/hellocd/NamehellocdApplication.java
@@ -1,13 +1,21 @@
 package com.example.hellocd;
-
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NamehellocdApplication {
+public class HellocdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NamehellocdApplication.class, args);
-	}
+
+		@RequestMapping("/")
+			String home() {
+						return "Hello World!";
+							}
+
+			public static void main(String[] args) {
+						SpringApplication.run(HellocdApplication.class, args);
+							}
 
 }
