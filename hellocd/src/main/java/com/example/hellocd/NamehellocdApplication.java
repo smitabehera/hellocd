bdcbaaf48f09fdb1574b1e1bcc826f63ab6ec599
smita <root@instance-2.asia-southeast1-b.c.ci-cd-275613.internal>
2020-12-18 12:44:46 +0000

    Firs commit all

---
+++ hellocd/src/main/java/com/example/hellocd/NamehellocdApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamehellocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamehellocdApplication.class, args);
+	}
+
+}
